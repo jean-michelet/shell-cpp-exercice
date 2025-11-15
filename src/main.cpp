d9991ca14fba6a009872afc1c356274cc7f0cfcc
jean <jean.antoine.michelet@gmail.com>
2025-11-15 08:16:37 +0100

    listen to user command input

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <string>
 
-int main() {
+int main()
+{
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   std::cout << "$ ";
+
+  std::string command;
+  std::getline(std::cin, command);
+
+  std::cout << command << ": command not found" << std::endl;
 }
