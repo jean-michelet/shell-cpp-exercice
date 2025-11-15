cb6301bd1d7e5bb6d5a9eaaf0b1dd6bfee6ede60
jean <jean.antoine.michelet@gmail.com>
2025-11-15 18:58:29 +0100

    ignore ide files

--- src/utils/find_exec.cpp
+++ src/utils/find_exec.cpp
@@ -4,12 +4,6 @@
 #include <filesystem>
 #include <unistd.h>
 
-#ifdef _WIN32
-constexpr char path_list_sep = ';';
-#else
-constexpr char path_list_sep = ':';
-#endif
-
 std::string find_exec(const std::string &cmd_name)
 {
     const char *raw = std::getenv("PATH");
@@ -23,7 +17,7 @@ std::string find_exec(const std::string &cmd_name)
 
     for (size_t i = 0; i <= path.size(); ++i)
     {
-        if (i == path.size() || path[i] == path_list_sep)
+        if (i == path.size() || path[i] == ':')
         {
             if (!current.empty())
             {
